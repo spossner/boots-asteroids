f8c3cb3db9e96160332f1f2610845ee070fa7f3a
spossner <github@possner.de>
2024-10-05 20:08:48 +0200

    initial commit with simple game loop filling the screen black and listening for window close event

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # poll for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False
+
+        # fill the screen with black to wipe away anything from last frame
+        screen.fill("black")
+
+        # RENDER YOUR GAME HERE
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
